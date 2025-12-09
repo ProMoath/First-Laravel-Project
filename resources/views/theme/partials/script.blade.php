c1c70687bc8e2f7b9a9d41a0a0b01c90e749d5e8
Moath Abdo <linktowindows00@gmail.com>
2025-12-09 23:05:02 +0300

    project - theme to views module

---
+++ resources/views/theme/partials/script.blade.php
@@ -0,0 +1,6 @@
+<script src="{{asset('assest')}}/vendors/jquery/jquery-3.2.1.min.js"></script>
+<script src="{{asset('assest')}}/vendors/bootstrap/bootstrap.bundle.min.js"></script>
+<script src="{{asset('assest')}}/vendors/owl-carousel/owl.carousel.min.js"></script>
+<script src="{{asset('assest')}}/js/jquery.ajaxchimp.min.js"></script>
+<script src="{{asset('assest')}}/js/mail-script.js"></script>
+<script src="{{asset('assest')}}/js/main.js"></script><?php
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ThemeController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +16,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::controller(ThemeController::class)->name('theme.')->group(function () {
+    Route::get('/', 'index')->name('index');
+    Route::get('/category', 'category')->name('category');
+    Route::get('/contact', 'contact')->name('contact');
+    Route::get('/single-blog', 'singleBlog')->name('singleBlog');
+    Route::get('/login', 'login')->name('login');
+    Route::get('/register', 'register')->name(' ');
+});
+
+/*
 Route::get('/', function () {
     return view('welcome');
 });
-
+*/
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
